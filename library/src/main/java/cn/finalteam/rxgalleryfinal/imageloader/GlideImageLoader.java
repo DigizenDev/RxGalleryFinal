3534dde5ef8290ea1e03b17cc403d5e2c0813aa3
dengyuhan <464958567@qq.com>
2017-05-22 10:35:19 +0800

    1.0.2.16 - 加快图片显示

--- library/src/main/java/cn/finalteam/rxgalleryfinal/imageloader/GlideImageLoader.java
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/imageloader/GlideImageLoader.java
@@ -9,7 +9,6 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.BitmapRequestBuilder;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.animation.ViewPropertyAnimation;
 
 import java.io.File;
@@ -22,25 +21,16 @@ public class GlideImageLoader implements AbsImageLoader {
     @Override
     public void displayImage(Object context, String path, ImageView imageView, Drawable defaultDrawable, Bitmap.Config config, boolean resize, int width, int height, int rotate) {
         Context ctx = (Context) context;
-        BitmapRequestBuilder<File, Bitmap> builder = null;
-        if (path != null) {
-            builder = Glide.with(ctx)
-                    .load(new File(path))
-                    .asBitmap()
-                    .placeholder(defaultDrawable);
-
-        } else {
-            builder = Glide.with(ctx)
-                    .load(new File("/sdcard"))
-                    .asBitmap()
-                    .placeholder(defaultDrawable);
-        }
+        BitmapRequestBuilder<File, Bitmap> builder = Glide.with(ctx)
+                .load(new File(path))
+                .asBitmap()
+                .placeholder(defaultDrawable);
         if (resize) {
             builder = builder.override(width, height);
         }
         builder.animate(new DrawableCrossFadeAnimator())
                 //.transform(new RotateTransformation(ctx, rotate))
-                .diskCacheStrategy(DiskCacheStrategy.NONE)
+                //.diskCacheStrategy(DiskCacheStrategy.NONE)
                 .into(imageView);
     }
 
@@ -50,18 +40,17 @@ public class GlideImageLoader implements AbsImageLoader {
         private static final int DEFAULT_DURATION_MS = 300;
 
 
-
         public DrawableCrossFadeAnimator() {
             this(DEFAULT_DURATION_MS);
         }
 
         public DrawableCrossFadeAnimator(int duration) {
-            this.duration=duration;
+            this.duration = duration;
         }
 
         @Override
         public void animate(View view) {
-            ObjectAnimator.ofFloat(view,"alpha",0f,1f).setDuration(duration).start();
+            ObjectAnimator.ofFloat(view, "alpha", 0f, 1f).setDuration(duration).start();
         }
     }
 
