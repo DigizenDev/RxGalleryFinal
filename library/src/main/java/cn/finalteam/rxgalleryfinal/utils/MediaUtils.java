d134162d00e4e003bd938ca979bcb457a6fdba84
dengyuhan <464958567@qq.com>
2017-06-06 21:45:04 +0800

    1.0.2.17 - 不存在的文件不添加到集合

--- library/src/main/java/cn/finalteam/rxgalleryfinal/utils/MediaUtils.java
+++ library/src/main/java/cn/finalteam/rxgalleryfinal/utils/MediaUtils.java
@@ -122,13 +122,13 @@ public class MediaUtils {
                     + " OR " + cn.finalteam.rxgalleryfinal.utils.MediaType.ofCommonVideoWhereSql() +
                     " AND " + MediaStore.MediaColumns.SIZE + ">0)";
             selectionArgs = cn.finalteam.rxgalleryfinal.utils.MediaType.ofCommonVideoWhereArgs(
-                    new String[]{bucketId,String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE),
-                    String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO)});
+                    new String[]{bucketId, String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE),
+                            String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO)});
         } else {
             //第一个显示所有视频
             selection = //MediaStore.Files.FileColumns.MEDIA_TYPE + "=?" + " OR " +
                     cn.finalteam.rxgalleryfinal.utils.MediaType.ofCommonVideoWhereSql() +
-                    " AND " + MediaStore.MediaColumns.SIZE + ">0";
+                            " AND " + MediaStore.MediaColumns.SIZE + ">0";
             selectionArgs = cn.finalteam.rxgalleryfinal.utils.MediaType.ofCommonVideoWhereArgs(new String[]{
                     //String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE),
                     String.valueOf(MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO)});
@@ -202,7 +202,9 @@ public class MediaUtils {
                 cursor.moveToFirst();
                 do {
                     MediaBean mediaBean = parseVideoCursorAndCreateThumImage(context, cursor);
-                    mediaBeanList.add(mediaBean);
+                    if (mediaBean != null) {
+                        mediaBeanList.add(mediaBean);
+                    }
                 } while (cursor.moveToNext());
             }
         }
@@ -354,12 +356,15 @@ public class MediaUtils {
      */
     private static MediaBean parseVideoCursorAndCreateThumImage(Context context, Cursor cursor) {
         MediaBean mediaBean = new MediaBean();
+        String originalPath = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+        mediaBean.setOriginalPath(originalPath);
+        if (!new File(originalPath).exists()) {
+            return null;
+        }
         long id = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media._ID));
         mediaBean.setId(id);
         String title = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.TITLE));
         mediaBean.setTitle(title);
-        String originalPath = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
-        mediaBean.setOriginalPath(originalPath);
         String bucketId = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.BUCKET_ID));
         mediaBean.setBucketId(bucketId);
         String bucketDisplayName = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.BUCKET_DISPLAY_NAME));
@@ -522,7 +527,7 @@ public class MediaUtils {
                 //获取数量
                 String[] tmp = Arrays.copyOf(selectionArgs, selectionArgs.length + 1);
                 tmp[tmp.length - 1] = bucketId;
-                Cursor c = contentResolver.query(uri, projection, "("+selection + ") and " + bucketKey + "=?", tmp, null);
+                Cursor c = contentResolver.query(uri, projection, "(" + selection + ") and " + bucketKey + "=?", tmp, null);
                 if (c != null && c.getCount() > 0) {
                     bucketBean.setImageCount(c.getCount());
                 }
